ce1f78136917f95bb9d4d8a2b4d9338d9b8137e1
hxy112292 <983771687@qq.com>
2019-09-29 16:38:18 -0500

    Add CrossOrigin

--- src/main/java/edu/uta/movietalk/controller/MovieController.java
+++ src/main/java/edu/uta/movietalk/controller/MovieController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author hxy
  */
-
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/movie")
 @RequiredArgsConstructor
--- src/main/java/edu/uta/movietalk/controller/UserController.java
+++ src/main/java/edu/uta/movietalk/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author housirvip
  */
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/user")
 @RequiredArgsConstructor
