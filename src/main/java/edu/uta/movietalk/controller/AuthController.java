7d19d9c996d400dd48662e28db4d4cafcc022b06
housirvip <housirvip@outlook.com>
2019-10-02 01:30:36 -0500

    delete useless code

--- src/main/java/edu/uta/movietalk/controller/AuthController.java
+++ src/main/java/edu/uta/movietalk/controller/AuthController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author housirvip
  */
-@CrossOrigin
 @RestController
 @RequestMapping(value = "/auth")
 @RequiredArgsConstructor
--- src/main/java/edu/uta/movietalk/controller/UserController.java
+++ src/main/java/edu/uta/movietalk/controller/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author housirvip
  */
-@CrossOrigin
 @RestController
 @RequestMapping(value = "/user")
 @RequiredArgsConstructor
