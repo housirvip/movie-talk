770dc532d2f81b52c98f4a68c9267b5e1121a2f5
housirvip <housirvip@outlook.com>
2019-10-02 01:40:50 -0500

    create a UserTests to be do

--- src/test/java/edu/uta/movietalk/AuthTests.java
+++ src/test/java/edu/uta/movietalk/AuthTests.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
--- src/test/java/edu/uta/movietalk/MovieTests.java
+++ src/test/java/edu/uta/movietalk/MovieTests.java
@@ -1,20 +1,14 @@
 package edu.uta.movietalk;
 
-import edu.uta.movietalk.controller.AuthController;
 import edu.uta.movietalk.controller.MovieController;
-import edu.uta.movietalk.controller.UserController;
-import edu.uta.movietalk.dto.UserDto;
-import edu.uta.movietalk.utils.JsonUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
