b9e2c088b0d20e01c0e6714a44a9e9f549b231d2
kong_p <kong_p@163.com>
2019-12-01 14:30:29 -0600

    junit test

--- src/test/java/edu/uta/movietalk/ReviewTests.java
+++ src/test/java/edu/uta/movietalk/ReviewTests.java
@@ -124,4 +124,56 @@ public class ReviewTests {
                 .andDo(MockMvcResultHandlers.print())
                 .andReturn();
     }
+    @Test
+    void testUpdateReview() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.put("/review")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    void testDeleteReview() throws Exception{
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/review")
+                .header("Authorization", "Bearer " + jwtToken)
+                .param("id","22"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    void testUpdateReviewReply() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.put("/review")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    void testCreateCollect() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/movie/collect")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    void testDeleteCollect() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.delete("/movie/collect")
+                .header("Authorization", "Bearer " + jwtToken)
+                .param("mid","475557"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+    @Test
+    void testUploadAvatar() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/avatar")
+                .header("Authorization", "Bearer " + jwtToken))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
+
 }
